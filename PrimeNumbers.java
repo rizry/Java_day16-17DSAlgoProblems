31ff162ba0bab140dea62819fd5c6c9505774d43
rizry <rizzwan.ansarii@gmail.com>
2022-10-03 16:25:05 +0530

    added prime palindrome and anagram check

--- PrimeNumbers.java
+++ PrimeNumbers.java
@@ -5,22 +5,57 @@ import java.util.ArrayList;
 public class PrimeNumbers {
 
   public static void find() {
-    generatePrimes();
+    ArrayList<Integer> primes = new ArrayList<>();
+    generatePrimes(primes);
+    System.out.println("primes: " + primes);
+
+    System.out.print("Primes that are palindrome: ");
+    for (int i : primes) {
+      if (i > 10 && i == getReverseNum(i)) {
+        System.out.print(i + " ");
+      }
+    }
+    System.out.print("\nPrimes that are anagrams: ");
+    for (int i : primes) {
+      int rev = getReverseNum(i);
+      if (i > 11 && checkPrime(rev)) {
+        System.out.print(i + " and " + rev + "; ");
+      }
+    }
+    System.out.print("\nPrimes that are both palindrome and anagrams: ");
+    for (int i : primes) {
+      int rev = getReverseNum(i);
+      if (i > 11 && i == getReverseNum(i) && checkPrime(rev)) {
+        System.out.print(i + " ");
+      }
+    }
+
   }
 
-  private static void generatePrimes() {
-    ArrayList<Integer> primes = new ArrayList<>();
+  private static void generatePrimes(ArrayList<Integer> primes) {
     for (int i = 1; i <= 1000; i++) {
-      boolean isPrime = true;
-      for (int j = 2; j <= Math.sqrt(i); j++) {
-        if (i % j == 0) {
-          isPrime = false;
-          break;
-        }
-
-      }
-      if (isPrime) primes.add(i);
+      if (checkPrime(i)) primes.add(i);
     }
-    System.out.println(primes);
+  }
+
+  private static int getReverseNum(int num) {
+    int rev = 0, r = 0;
+    while (num > 0) {
+      r = num % 10;
+      rev = rev * 10 + r;
+      num /= 10;
+    }
+    return rev;
+  }
+
+  private static boolean checkPrime(int num) {
+    boolean isPrime = true;
+    for (int j = 2; j <= Math.sqrt(num); j++) {
+      if (num % j == 0) {
+        isPrime = false;
+        break;
+      }
+    }
+    return isPrime;
   }
 }
